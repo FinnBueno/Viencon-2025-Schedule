5ffd12a31a7c1b571fa67fd2d64deb044d11805b
Finn Bon <f.bon@coolblue.nl>
2025-08-18 14:46:03 +0200

    configure locations

---
+++ src/assets/locations.ts
@@ -0,0 +1,44 @@
+type Location = {
+  name: string;
+  subroom?: { [key: string]: Location };
+};
+
+export const LOCATIONS: { [key: string]: Location } = {
+  Mainstage: {
+    name: "Mainstage",
+    subroom: {
+      roomOne: {
+        name: "Room 1",
+      },
+      roomTwo: {
+        name: "Room 2",
+      },
+    },
+  },
+  DuckyDomeStage: {
+    name: "Ducky Dome Stage",
+  },
+  Gameroom: {
+    name: "Gameroom/Karaoke",
+  },
+  SharkHouse: {
+    name: "Sharkhouse",
+  },
+  MainstageField: {
+    name: "Field next to Mainstage",
+  },
+  HawkArena: {
+    name: "Hawk Arena",
+    subroom: {
+      gym: {
+        name: "Gym Hall",
+      },
+      squash: {
+        name: "Squash Hall",
+      },
+    },
+  },
+  Pool: {
+    name: "Pool",
+  },
+};
